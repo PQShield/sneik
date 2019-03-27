b81a79f17b04e2f68c5ea98664e618b385cf0d1d
Markku-Juhani O. Saarinen <mjos@iki.fi>
2019-03-27 14:16:47 +0000

    update

--- common/nist/crypto_hash.h
+++ common/nist/crypto_hash.h
@@ -1,7 +1,7 @@
 
 int crypto_hash(
-	unsigned char *out,
-	const unsigned char *in,
-	unsigned long long inlen
-	);
+    unsigned char *out,
+    const unsigned char *in,
+    unsigned long long inlen
+    );
 
